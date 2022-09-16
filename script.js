c1b8a1879808cb37766091637bf2c6d30378dd3e
Brandon <106634008+brandon31@users.noreply.github.com>
2022-09-16 23:43:59 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const playButton = document.getElementById('play');
+const pauseButton = document.getElementById('pause');
+const stopButton = document.getElementById('stop');
+
+const textInput = document.getElementById('text-input');
+
+const speedInput = document.getElementById('speed');
+
+playButton.addEventListener('click', () => {
+    playText(textInput.value);
+})
+
+pauseButton.addEventListener('click', pauseText);
+
+stopButton.addEventListener('click', stopText);
+
+speedInput.addEventListener('input', () => {
+    stopText();
+    playText(utterance.text.substring(currentCharacter))
+})
+
+const utterance = new SpeechSynthesisUtterance();utterance.addEventListener('end', () => {
+    textInput.disabled = false;
+})
+utterance.addEventListener('boundary', e => {
+    currentCharacter = e.charIndex
+})
+
+function playText(text) {
+
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume()
+    }
+    if (speechSynthesis.speaking) return
+    utterance.text = text;
+    utterance.rate = speedInput.value || 1;
+    textInput.disabled = true;
+    speechSynthesis.speak(utterance);
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) {
+        speechSynthesis.pause()
+    }
+}
+
+function stopText() {
+    speechSynthesis.resume() 
+    speechSynthesis.cancel()
+}
